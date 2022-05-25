6e38587ab85fa4a50f8e938cf3819d53e3f7f710
Jaka Daneu <jakad@me.com>
2022-05-25 17:45:21 +0200

    feat: 🌐 internationalized routing
    Slovenian -default
    English
    Italian

---
+++ next-i18next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  i18n: {
+    locales: ["default", "sl", "en", "it"],
+    defaultLocale: "default",
+    localeDetection: false,
+  },
+};
--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+const { i18n } = require("./next-i18next.config");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
+  i18n,
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
