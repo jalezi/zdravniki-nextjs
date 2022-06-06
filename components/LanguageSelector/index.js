d60f4924634e52541d0953fed67a6367eefd1d5b
Jaka Daneu <jakad@me.com>
2022-06-06 18:19:34 +0200

    refactor(components): ♻️ react-select lng selector

--- components/LanguageSelector/index.js
+++ components/LanguageSelector/index.js
@@ -1,11 +1,11 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useRouter } from "next/router";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { Controller, useForm } from "react-hook-form";
-import styled, { css } from "styled-components";
+import Select from "react-select";
 
-import useOutsideClick from "../../hooks/useOutsideClick";
+import { theme as appTheme } from "../../constants/theme";
+
+import { customStyles } from "./styles";
 
 const LANGUAGES_MAP = {
   sl: "Slovenščina",
@@ -13,164 +13,14 @@ const LANGUAGES_MAP = {
   it: "Italiano",
 };
 
-export const baseInput = css`
-  padding-block: 4px;
-  padding-left: 4px;
-  padding-right: 0;
-  border-radius: 4px;
-  width: 100%;
-  color: ${(props) => props.theme.textColor2};
-  ${(props) => props.theme.medium_14};
-  ${(props) => {
-    if (props.error)
-      return css`
-        border: solid ${props.theme.error} 1px;
-      `;
-    return css`
-      border: solid ${props.theme.textColor2} 1px;
-      &:focus {
-        border: solid ${props.theme.texColor1} 1px;
-        color: ${props.theme.textColor1};
-      }
-    `;
-  }}
-
-  outline: none;
-`;
-
-const Container = styled.div`
-  .hidden {
-    display: none;
-  }
-
-  min-width: 45px;
-
-  .custom-select-wrapper {
-    position: relative;
-    user-select: none;
-    width: 100%;
-  }
-  .custom-select {
-    ${baseInput}
-    background: transparent;
-    position: relative;
-    cursor: pointer;
-    color: ${(props) => props.theme.textColor2};
-  }
-  .custom-select__trigger {
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    cursor: pointer;
-  }
-  .custom-options {
-    position: absolute;
-    display: none;
-    top: 100%;
-    left: -110%;
-    right: 0;
-    background: ${(props) => props.theme.bgColor1};
-    transition: all 5s;
-    pointer-events: none;
-    z-index: 2;
-    padding: 8px;
-    width: fit-content;
-    max-height: 200px;
-    overflow-y: auto;
-  }
-  .custom-select.open .custom-options {
-    display: block;
-    pointer-events: all;
-    margin-top: 8px;
-    box-shadow: -1px 1px 2px rgba(67, 70, 74, 0.0001),
-      -2px 2px 5px rgba(67, 86, 100, 0.123689);
-    border-radius: 4px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: fit-content;
-  }
-  .custom-option {
-    position: relative;
-    display: block;
-    padding: 4px;
-    cursor: pointer;
-    transition: all 0.5s;
-    color: ${(props) => props.theme.textColor1};
-    /* margin-bottom: 12px; */
-    /* height: 32px; */
-  }
-  padding-block: 4px;
-
-  .option-container {
-    width: 100%;
-    text-align: right;
-  }
-
-  .option-container:hover {
-    .custom-option {
-      cursor: pointer;
-      background-color: ${(props) => props.theme.grey6};
-    }
-  }
-
-  .custom-option.selected {
-    color: ${(props) => props.theme.textColor1};
-    background-color: ${(props) => props.theme.grey4};
-  }
-
-  .arrow {
-    position: relative;
-    height: 7.72px;
-    width: 12.26px;
-    margin-left: 8px;
-  }
-  .arrow::before,
-  .arrow::after {
-    content: "";
-    position: absolute;
-    bottom: 0px;
-    width: 0.15rem;
-    height: 100%;
-    transition: all 0.5s;
-  }
-  .arrow::before {
-    left: -2px;
-    transform: rotate(-45deg);
-    background-color: ${(props) => props.theme.textColor2};
-  }
-  .arrow::after {
-    left: 2px;
-    transform: rotate(45deg);
-    background-color: ${(props) => props.theme.textColor2};
-  }
-  .open .arrow::before {
-    left: -2px;
-    transform: rotate(45deg);
-  }
-  .open .arrow::after {
-    left: 2px;
-    transform: rotate(-45deg);
-  }
-`;
-
 export default function LanguageSelector() {
   const router = useRouter();
-
-  const [isOpen, setIsOpen] = useState(false);
-  const [selected, setSelected] = useState(router.locale);
-
   const hiddenInputRef = useRef();
-  const customSelectRef = useRef();
-  useOutsideClick(customSelectRef, () => {
-    setIsOpen(false);
-  });
 
   const { handleSubmit, control, setValue } = useForm({
     defaultValues: { lngSelector: router.locale },
   });
+
   const options = router.locales
     .filter((lng) => lng !== "default")
     .map((lng) => ({ value: lng, label: LANGUAGES_MAP[lng] }));
@@ -179,73 +29,40 @@ export default function LanguageSelector() {
     router.push(router.pathname, router.pathname, { locale: data.lngSelector });
   };
 
-  const onLngChange = (e, value) => {
-    setSelected(value);
-    setValue("lngSelector", value);
+  const onLngChange = (option) => {
+    setValue("lngSelector", option.value);
     hiddenInputRef.current.click();
   };
 
-  // TODO: where should I put "aria-label='Selected language: English'"
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Controller
         control={control}
         name="lngSelector"
         defaultValue={router.locale}
-        render={({ field: { value, name } }) => (
-          <Container>
-            <select
-              id={name}
-              name={name}
-              value={value}
-              onChange={onLngChange}
-              className="hidden"
-            >
-              {options.map((item) => (
-                <option key={item.value} value={item.value}>
-                  {item.label}
-                </option>
-              ))}
-            </select>
-            <div
-              ref={customSelectRef}
-              onClick={() => {
-                setIsOpen((prev) => !prev);
-              }}
-              className="custom-select-wrapper"
-            >
-              <div className={`custom-select ${isOpen && "open"}`}>
-                <div className="custom-select__trigger">
-                  <span>
-                    {options
-                      .find((item) => item.value === selected)
-                      ?.value.toUpperCase() || "Select"}
-                  </span>
-                  <div className="arrow" />
-                </div>
-                <div className="custom-options" role="menuitem" tabIndex="0">
-                  {options.map((item) => (
-                    <div
-                      key={item.value}
-                      onClick={(e) => {
-                        onLngChange(e, item.value);
-                      }}
-                      className="option-container"
-                    >
-                      <span
-                        className={`custom-option ${
-                          selected === item.value && "selected"
-                        } `}
-                        data-value={item.value}
-                      >
-                        {item.label}
-                      </span>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </Container>
+        render={({ field: { value, name, onBlur } }) => (
+          <Select
+            id="lang-selector"
+            instanceId="lang-selector"
+            options={options}
+            defaultValue={{ value, label: LANGUAGES_MAP[value] }}
+            name={name}
+            onChange={onLngChange}
+            onBlur={onBlur}
+            formatOptionLabel={(option, { context }) =>
+              context === "value" ? option.value.toUpperCase() : option.label
+            }
+            aria-label={`Selected language: ${LANGUAGES_MAP[value]}`}
+            isSearchable={false}
+            styles={customStyles}
+            theme={(reactSelectTheme) => ({
+              ...reactSelectTheme,
+              appTheme,
+              colors: {
+                ...reactSelectTheme.colors,
+              },
+            })}
+          />
         )}
       />
 
---
+++ components/LanguageSelector/styles.js
@@ -0,0 +1,55 @@
+export const customStyles = {
+  control: (provided, { theme, isFocused }) => ({
+    ...provided,
+    background: "transparent",
+    cursor: "pointer",
+    borderColor: isFocused ? "transparent" : theme.appTheme.textColor2,
+    minHeight: "unset",
+    minWidth: 69.5,
+    boxShadow: isFocused ? `0 0 0 1px ${theme.appTheme.bgColor1}` : "none",
+    opacity: isFocused ? 1 : 0.7,
+    "&:hover": {
+      borderColor: isFocused ? "transparent" : theme.appTheme.textColor1,
+      opacity: 1,
+    },
+  }),
+  indicatorSeparator: (provided, { theme }) => ({
+    ...provided,
+    backgroundColor: theme.appTheme.textColor2,
+  }),
+  dropdownIndicator: (provided, { theme }) => ({
+    ...provided,
+    color: theme.appTheme.textColor2,
+    paddingBlock: "0.25rem",
+    paddingInline: "0.25rem",
+    "&:hover": {
+      color: theme.appTheme.textColor1,
+    },
+  }),
+  menu: (provided) => ({
+    ...provided,
+    width: "auto",
+    minWidth: "100%",
+    marginLeft: "-60%",
+    padding: 8,
+  }),
+  option: (provided, { isFocused, isSelected, theme }) => {
+    let bgColor = isSelected ? theme.appTheme.brand : undefined;
+
+    if (!bgColor) {
+      bgColor = isFocused ? theme.appTheme.grey5 : undefined;
+    }
+
+    return {
+      ...provided,
+      cursor: "pointer",
+      backgroundColor: bgColor,
+    };
+  },
+  singleValue: (provided, { theme }) => ({
+    ...provided,
+    color: theme.appTheme.textColor2,
+    textAlign: "center",
+    "&:hover": { color: theme.appTheme.textColor1 },
+  }),
+};
