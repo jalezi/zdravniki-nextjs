ed0ddd70462dea235acf08b6a92b995d9fc843d2
Jaka Daneu <jakad@me.com>
2022-06-09 09:45:45 +0200

    feat(pages): 🚧 start with Faq page

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,8 +1,10 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
 import Header from "../../components/Header";
 import SEO from "../../components/SEO";
+import { GlossaryPropType, QuestionPropType } from "../../types";
 
 const StyledMain = styled.main`
   height: calc(100% - 56px);
@@ -12,7 +14,34 @@ const StyledMain = styled.main`
   }
 `;
 
+const Section = function Section({ sectionData }) {
+  return sectionData.map((section) => (
+    <div key={`${section.position}-faq`}>
+      <p>{section.position}</p>
+      <h2>{section.question ?? section.term}</h2>
+      <p>{section.answer ?? section.definition}</p>
+      <p>{section.slug}</p>
+    </div>
+  ));
+};
+
+Section.propTypes = {
+  sectionData: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.shape(QuestionPropType)),
+    PropTypes.arrayOf(PropTypes.shape(GlossaryPropType)),
+  ]),
+};
+
 export async function getStaticProps({ locale }) {
+  if (locale === "default") {
+    return { notFound: true };
+  }
+
+  const response = await fetch(
+    `${process.env.CONTENT_ENDPOINT_BASE}/faq/3/?lang=${locale}`
+  );
+  const data = await response.json();
+
   const PUBLIC_URL = process.env.PUBLIC_URL ?? null;
 
   return {
@@ -20,16 +49,32 @@ export async function getStaticProps({ locale }) {
       ...(await serverSideTranslations(locale, ["common", "header"])),
       // Will be passed to the page component as props
       url: PUBLIC_URL,
+      data,
     },
+    revalidate: 10,
   };
 }
 
-export default function Faq() {
+export default function Faq({ url, data }) {
   return (
     <>
-      <SEO title="About" />
+      <SEO title="FAQ" description="Some description" url={url} />
       <Header />
-      <StyledMain>Faq</StyledMain>
+      <StyledMain>
+        {/* <Section sectionData={data.faq} /> */}
+        <Section sectionData={data.glossary} />
+      </StyledMain>
     </>
   );
 }
+
+Faq.propTypes = {
+  url: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    faq: PropTypes.arrayOf(PropTypes.shape(QuestionPropType)),
+    glossary: PropTypes.arrayOf(PropTypes.shape(GlossaryPropType)),
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    resource_uri: PropTypes.string.isRequired,
+  }).isRequired,
+};
--- types/index.js
+++ types/index.js
@@ -4,3 +4,17 @@ export const ChildrenPropType = PropTypes.oneOfType([
   PropTypes.arrayOf(PropTypes.node),
   PropTypes.node,
 ]);
+
+export const QuestionPropType = {
+  question: PropTypes.string.isRequired,
+  answer: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
+  position: PropTypes.number.isRequired,
+};
+
+export const GlossaryPropType = {
+  definition: PropTypes.string,
+  term: PropTypes.string,
+  slug: PropTypes.string,
+  position: PropTypes.number,
+};
