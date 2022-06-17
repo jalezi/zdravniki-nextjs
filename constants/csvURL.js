6e2021106e23637b20ad54a77857fc4d58e841a3
Jaka Daneu <jakad@me.com>
2022-06-17 23:56:12 +0200

    build: ✨ add fetch func for raw and json
    BREAKING CHANGE: use node gte v17.5.0 - built-in fetch

---
+++ constants/csvURL.js
@@ -0,0 +1,15 @@
+const ORIGIN = "https://raw.githubusercontent.com";
+const BASE_URL = new URL(ORIGIN);
+
+const BASE_PATH = "/sledilnik/zdravniki-data/main/csv";
+
+const INSTITUTIONS_CSV_PATH = `${BASE_PATH}/institutions.csv`;
+const DOCTORS_CSV_PATH = `${BASE_PATH}/doctors.csv`;
+
+const DOCTORS_TS_PATH = `${BASE_PATH}/doctors.csv.timestamp`;
+const INSTITUTIONS_TS_PATH = `${BASE_PATH}/institutions.csv.timestamp`;
+
+export const INSTITUTIONS_CSV_URL = new URL(INSTITUTIONS_CSV_PATH, BASE_URL);
+export const DOCTORS_CSV_URL = new URL(DOCTORS_CSV_PATH, BASE_URL);
+export const DOCTORS_TS_URL = new URL(DOCTORS_TS_PATH, BASE_URL);
+export const INSTITUTIONS_TS_URL = new URL(INSTITUTIONS_TS_PATH, BASE_URL);
