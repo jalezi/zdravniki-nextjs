83925501ecb179dee69f89745de4601e57a355f0
Jaka Daneu <jakad@me.com>
2022-06-22 21:16:02 +0200

    refactor(components): 🚚 move Map to separate file

---
+++ components/Map/Map.js
@@ -0,0 +1,48 @@
+import PropTypes from "prop-types";
+import { AttributionControl, TileLayer, MapContainer } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
+
+import { MAP } from "../../constants/common";
+import { ChildrenPropType, StylePropType } from "../../types/index";
+
+export function Map({ children, center, zoom, style }) {
+  return (
+    <MapContainer
+      center={center}
+      zoom={zoom}
+      attributionControl={false}
+      style={style}
+    >
+      <AttributionControl prefix="" />
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {children}
+    </MapContainer>
+  );
+}
+
+Map.defaultProps = {
+  children: undefined,
+  center: MAP.GEO_LOCATION.SL_CENTER,
+  style: undefined,
+  zoom: MAP.ZOOM,
+};
+
+Map.propTypes = {
+  children: ChildrenPropType,
+  center: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.number),
+    PropTypes.shape({
+      lat: PropTypes.number,
+      lng: PropTypes.number,
+      alt: PropTypes.number,
+    }),
+  ]),
+  style: StylePropType,
+  zoom: PropTypes.number,
+};
--- components/Map/index.js
+++ components/Map/index.js
@@ -1,57 +1,10 @@
 import PropTypes from "prop-types";
-import { AttributionControl, TileLayer, MapContainer } from "react-leaflet";
-
-import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
-import "leaflet-defaulticon-compatibility";
 
 import { MAP } from "../../constants/common";
-import {
-  ChildrenPropType,
-  DoctorPropType,
-  StylePropType,
-} from "../../types/index";
+import { DoctorPropType } from "../../types/index";
 
+import { Map } from "./Map";
 import { DoctorMarker } from "./Markers";
-
-export function Map({ children, center, zoom, style }) {
-  return (
-    <MapContainer
-      center={center}
-      zoom={zoom}
-      attributionControl={false}
-      style={style}
-    >
-      <AttributionControl prefix="" />
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {children}
-    </MapContainer>
-  );
-}
-
-Map.defaultProps = {
-  children: undefined,
-  center: MAP.GEO_LOCATION.SL_CENTER,
-  style: undefined,
-  zoom: MAP.ZOOM,
-};
-
-Map.propTypes = {
-  children: ChildrenPropType,
-  center: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.number),
-    PropTypes.shape({
-      lat: PropTypes.number,
-      lng: PropTypes.number,
-      alt: PropTypes.number,
-    }),
-  ]),
-  style: StylePropType,
-  zoom: PropTypes.number,
-};
 
 const withMap = function withMap(Component) {
   const DoctorsMap = function DoctorsMap({ center, zoom, doctors }) {
