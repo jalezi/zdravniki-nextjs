19de286f7b3d74bda10be54e258e3b064e652d6e
Jaka Daneu <jakad@me.com>
2022-07-01 07:26:24 +0200

    feat(DoctorCards): ✨ use Info's styles
    to display doctors. Missing more button icon, tooltips,
    phone button as a with tel as href.

--- components/DoctorCards/index.js
+++ components/DoctorCards/index.js
@@ -2,34 +2,44 @@ import Link from 'next/link';
 
 import PropTypes from 'prop-types';
 
-import { toSlug } from '../../lib';
+import { toPercent } from '../../lib/helpers';
 import { DoctorPropType } from '../../types';
+import { PhoneBigIcon, PhoneNoneBigIcon } from '../Shared/Icons';
+import Accepts from './Accepts';
+import CircleChart from './CircleChart';
+import * as Styled from './styles';
 
 const DoctorCards = function DoctorCards({ doctors }) {
   const drJsx = dr => (
-    <div key={dr.doctor + dr.id_inst}>
-      <br />
-      {Object.entries(dr).map(([key, value]) => {
-        if (key === 'doctor') {
-          return (
-            <p key={`${value}${key}S`}>
-              <span>{key}: </span>
-              <Link href={`/gp/${toSlug(value)}`} key={`${value}${key}L`}>
-                {value}
-              </Link>
-            </p>
-          );
-        }
-        return (
-          <p key={`${value}${key}M`}>
-            {key}: {value}
-          </p>
-        );
-      })}
-      <br />
-    </div>
+    <Styled.InfoContainer as="article" key={dr.name + dr.instId}>
+      <Styled.InfoContent>
+        <Styled.Name as="h3">
+          <Link href={`/gp/${dr.nameSlug}`}>{dr.name}</Link>
+        </Styled.Name>
+        <Styled.Provider>{dr.provider}</Styled.Provider>
+        <Styled.Address>{dr.fullAddress}</Styled.Address>
+        <Styled.InfoSubContent>
+          <Styled.AcceptsContainer>
+            <Accepts accepts={dr.accepts} />
+          </Styled.AcceptsContainer>
+          <CircleChart size="26px" percent={dr.availability} />
+          <Styled.AvailabilityText>
+            {toPercent(dr.availability)}
+          </Styled.AvailabilityText>
+        </Styled.InfoSubContent>
+      </Styled.InfoContent>
+      <Styled.InfoActions>
+        <Styled.IconButtonBase>E</Styled.IconButtonBase>
+        <Styled.IconButtonBase
+          phone={dr.phone}
+          disabled={dr.phone ? undefined : true}
+        >
+          {dr.phone ? <PhoneBigIcon /> : <PhoneNoneBigIcon />}
+        </Styled.IconButtonBase>
+      </Styled.InfoActions>
+    </Styled.InfoContainer>
   );
-  return <div style={{ overflow: 'auto' }}>{doctors.map(drJsx)}</div>;
+  return <>{doctors.map(drJsx)}</>;
 };
 
 DoctorCards.propTypes = {
