284a34abced8b6ab37c7ff603913de0f09557946
Jaka Daneu <jakad@me.com>
2022-07-01 14:35:30 +0200

    feat(components): ⚡️ add infinite list scroll

--- components/DoctorCard/Info/index.js
+++ components/DoctorCard/Info/index.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+import { forwardRef } from 'react';
+
 import { toPercent } from '../../../lib/helpers';
 import { DoctorPropType } from '../../../types';
 import { PhoneBigIcon, PhoneNoneBigIcon } from '../../Shared/Icons';
@@ -7,39 +9,41 @@ import Accepts from '../Accepts';
 import CircleChart from '../CircleChart';
 import * as Styled from './styles';
 
-const Info = function Info({ doctor }) {
-  return (
-    <Styled.InfoContainer as="article" key={doctor.name + doctor.instId}>
-      <Styled.InfoContent>
-        <Styled.Name as="h3">
-          <Link href={`/gp/${doctor.nameSlug}`}>{doctor.name}</Link>
-        </Styled.Name>
-        <Styled.Provider>{doctor.provider}</Styled.Provider>
-        <Styled.Address>{doctor.fullAddress}</Styled.Address>
-        <Styled.InfoSubContent>
-          <Styled.AcceptsContainer>
-            <Accepts accepts={doctor.accepts} />
-          </Styled.AcceptsContainer>
-          <CircleChart size="26px" percent={doctor.availability} />
-          <Styled.AvailabilityText>
-            {toPercent(doctor.availability)}
-          </Styled.AvailabilityText>
-        </Styled.InfoSubContent>
-      </Styled.InfoContent>
-      <Styled.InfoActions>
-        <Styled.IconButtonBase>E</Styled.IconButtonBase>
-        <Styled.IconButtonBase
-          as={doctor.phone ? 'a' : 'button'}
-          href={doctor.phone ? `tel:${doctor.phone}` : undefined}
-          phone={doctor.phone}
-          disabled={doctor.phone ? undefined : true}
-        >
-          {doctor.phone ? <PhoneBigIcon /> : <PhoneNoneBigIcon />}
-        </Styled.IconButtonBase>
-      </Styled.InfoActions>
-    </Styled.InfoContainer>
-  );
-};
+const Info = forwardRef(({ doctor }, ref) => (
+  <Styled.InfoContainer
+    ref={ref}
+    as="article"
+    key={doctor.name + doctor.instId}
+  >
+    <Styled.InfoContent>
+      <Styled.Name as="h3">
+        <Link href={`/gp/${doctor.nameSlug}`}>{doctor.name}</Link>
+      </Styled.Name>
+      <Styled.Provider>{doctor.provider}</Styled.Provider>
+      <Styled.Address>{doctor.fullAddress}</Styled.Address>
+      <Styled.InfoSubContent>
+        <Styled.AcceptsContainer>
+          <Accepts accepts={doctor.accepts} />
+        </Styled.AcceptsContainer>
+        <CircleChart size="26px" percent={doctor.availability} />
+        <Styled.AvailabilityText>
+          {toPercent(doctor.availability)}
+        </Styled.AvailabilityText>
+      </Styled.InfoSubContent>
+    </Styled.InfoContent>
+    <Styled.InfoActions>
+      <Styled.IconButtonBase>E</Styled.IconButtonBase>
+      <Styled.IconButtonBase
+        as={doctor.phone ? 'a' : 'button'}
+        href={doctor.phone ? `tel:${doctor.phone}` : undefined}
+        phone={doctor.phone}
+        disabled={doctor.phone ? undefined : true}
+      >
+        {doctor.phone ? <PhoneBigIcon /> : <PhoneNoneBigIcon />}
+      </Styled.IconButtonBase>
+    </Styled.InfoActions>
+  </Styled.InfoContainer>
+));
 
 export default Info;
 
--- components/DoctorCards/index.js
+++ components/DoctorCards/index.js
@@ -1,3 +1,5 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
 import { useTranslation } from 'next-i18next';
 import PropTypes from 'prop-types';
 
@@ -5,22 +7,77 @@ import { DoctorPropType } from '../../types';
 import Info from '../DoctorCard/Info';
 import * as Styled from './styles';
 
+const getGroupsByLetter = (acc, doctor) => {
+  const firstLetter = doctor.name.charAt(0).toUpperCase();
+  if (!acc[firstLetter]) {
+    acc[firstLetter] = [];
+  }
+  acc[firstLetter].push(doctor);
+  return acc;
+};
+
+function useInfiniteScroll({ doctors, pageNum }) {
+  const [list, setList] = useState([]);
+  const hasMore = doctors.length > list.length;
+
+  useEffect(() => {
+    setList(doctors.slice(0, pageNum * 20));
+  }, [doctors, pageNum]);
+
+  return { list, hasMore };
+}
+
 const DoctorCards = function DoctorCards({ doctors }) {
   const { t: tMap } = useTranslation('map');
+  const [pageNum, setPageNum] = useState(1);
+  const observer = useRef();
 
-  const drJsx = dr => <Info doctor={dr} />;
+  const { list, hasMore } = useInfiniteScroll({
+    doctors,
+    pageNum,
+  });
 
-  const groupedByLetter = doctors.reduce((acc, doctor) => {
-    const firstLetter = doctor.name.charAt(0).toUpperCase();
+  const lastBookElementRef = useCallback(
+    // (*)
+    node => {
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNum(prev => prev + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [hasMore]
+  );
 
-    if (!acc[firstLetter]) {
-      acc[firstLetter] = [];
-    }
+  const groupedByLetter = list.reduce(getGroupsByLetter, {});
 
-    acc[firstLetter].push(doctor);
+  const getVisibleCards = (obj, length) => {
+    let elementCount = 0;
+    return Object.entries(obj).map(([letter, drGroup]) => {
+      const cards = drGroup.map(dr => {
+        elementCount += 1;
 
-    return acc;
-  }, {});
+        return (
+          <Info
+            key={`${elementCount}-${dr.name}`}
+            doctor={dr}
+            ref={elementCount === length ? lastBookElementRef : undefined}
+          />
+        );
+      });
+
+      return (
+        <section key={letter}>
+          <Styled.HeadingBase as="h2">{letter}</Styled.HeadingBase>
+          {cards}
+        </section>
+      );
+    });
+  };
+
+  const visibleCards = getVisibleCards(groupedByLetter, list.length);
 
   const count = doctors.length;
 
@@ -29,14 +86,7 @@ const DoctorCards = function DoctorCards({ doctors }) {
       <Styled.Header>
         <span>{tMap('totalResults', { count })}</span>
       </Styled.Header>
-      <Styled.DoctorsContainer>
-        {Object.entries(groupedByLetter).map(([letter, drGroup]) => (
-          <section key={letter}>
-            <Styled.HeadingBase as="h2">{letter}</Styled.HeadingBase>
-            {drGroup.map(drJsx)}
-          </section>
-        ))}
-      </Styled.DoctorsContainer>
+      <Styled.DoctorsContainer>{visibleCards}</Styled.DoctorsContainer>
     </>
   );
 };
