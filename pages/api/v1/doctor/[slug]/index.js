b732b0fae7ba5b4cd4b862624cf3d1840dc739be
Jaka Daneu <jakad@me.com>
2022-07-03 11:50:10 +0200

    feat(api/v1): ✨ add route /doctor/:slug
    Returns array of doctors either cached or fetched.

---
+++ pages/api/v1/doctor/[slug]/index.js
@@ -0,0 +1,67 @@
+import { DOCTORS_TS_URL } from '../../../../../constants/csvURL';
+import { getNowToLocaleString, toSlug } from '../../../../../lib';
+
+const cache = new Map();
+
+const notExistCache = new Map();
+
+const NOT_FOUND_MAP_VALUE = '404 - Not Found';
+
+// TODO Error handling
+export default async function handler(req, res) {
+  const { slug } = req.query;
+  const metadata = { url: req.url, time: getNowToLocaleString() };
+
+  const responseTs = await fetch(DOCTORS_TS_URL);
+  const ts = await responseTs.json();
+  const isInNotExistCache = Boolean(notExistCache.get(slug)?.get(ts));
+
+  if (isInNotExistCache) {
+    const error = new Error("Doctor with this slug doesn't exist");
+    return res.status(200).json({
+      success: false,
+      error: { code: 404, message: error.message },
+      metadata: { ...metadata, cached: true },
+    });
+  }
+
+  const cached = cache.get(slug)?.get(ts);
+
+  if (cached?.length) {
+    return res.status(200).json({
+      data: cached,
+      slug,
+      success: true,
+      updatedAt: ts,
+      metadata: { ...metadata, cached: true, length: cached.length },
+    });
+  }
+
+  const response = await fetch(`${process.env.PUBLIC_URL}/api/v1/doctors`);
+  const { data } = await response.json();
+  const doctors = data.filter(doctor => toSlug(doctor.doctor) === slug);
+
+  if (doctors.length === 0) {
+    notExistCache.set(slug, new Map([[ts, NOT_FOUND_MAP_VALUE]]));
+
+    const error = new Error("Doctor with this slug doesn't exist");
+    return res.status(200).json({
+      success: false,
+      error: { code: 404, message: error.message },
+      metadata: { ...metadata, cached: false },
+    });
+  }
+
+  cache.set(slug, new Map([[ts, doctors]]));
+
+  return res.status(200).json(
+    {
+      data: doctors,
+      slug,
+      success: true,
+      updatedAt: ts,
+      metadata: { ...metadata, cached: false, length: doctors.length },
+    },
+    slug
+  );
+}
