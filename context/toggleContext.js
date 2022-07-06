67776c6de988446ea022a5de5cffcab0912a7f80
Jaka Daneu <jakad@me.com>
2022-07-06 19:28:10 +0200

    style: 🚨 lint import order

--- context/toggleContext.js
+++ context/toggleContext.js
@@ -1,5 +1,6 @@
-import PropTypes from 'prop-types';
 import { createContext, useState, useContext, useMemo } from 'react';
+
+import PropTypes from 'prop-types';
 
 import { ChildrenPropType } from '../types';
 
