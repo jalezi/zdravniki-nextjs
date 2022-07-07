8072de06ade9bae5d312966c3ff2e401538b8c3b
Jaka Daneu <jakad@me.com>
2022-07-07 06:00:59 +0200

    fix: 🐛 no title and description in faq & about

--- layouts/MDXLayout/index.js
+++ layouts/MDXLayout/index.js
@@ -1,16 +1,13 @@
-import PropTypes from 'prop-types';
 
 import Header from '../../components/Header';
-import SEO from '../../components/SEO';
 import ScrollToTop from '../../components/Shared/ScrollToTop';
 import { Button } from '../../components/Shared/ScrollToTop/styles';
 import { ChildrenPropType } from '../../types';
 import * as Styled from './styles';
 
-export default function MDXLayout({ children, title, description }) {
+export default function MDXLayout({ children }) {
   return (
     <>
-      <SEO title={title} description={description} />
       <Header />
       <Styled.CustomContainer>
         <Styled.StaticPageWrapper>
@@ -23,7 +20,5 @@ export default function MDXLayout({ children, title, description }) {
 }
 
 MDXLayout.propTypes = {
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
   children: ChildrenPropType.isRequired,
 };
