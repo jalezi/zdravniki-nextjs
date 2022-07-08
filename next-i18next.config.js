87479f843b84b27df65032ac226b7049d9e438a5
Jaka Daneu <jakad@me.com>
2022-07-08 12:14:12 +0200

    fix: 🐛 ssr translation not working
    _error page

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,12 +2,14 @@
 /**
  * For example, here's a next.config.js file with support for a few languages. Note the "default" locale has been added intentionally.
  */
+const path = require('path');
 
 module.exports = {
   i18n: {
     locales: ['default', 'sl', 'en', 'it'],
     defaultLocale: 'default',
     localeDetection: false,
+    localePath: path.resolve('./public/locales'),
     fallbackLng: 'sl',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
