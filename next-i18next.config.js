aadcdac3cea5a6510f8e3a65dedb9beee1cc73f6
Jaka Daneu <jakad@me.com>
2022-08-10 18:23:15 +0200

    build(vercel): 🚑️ error serverless crashed
    dynamic sub pages /[type]/[doctorName]/[instId]

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,12 +3,13 @@
  * For example, here's a next.config.js file with support for a few languages. Note the "default" locale has been added intentionally.
  */
 
-// const path = require('path');
+const path = require('path');
 
 module.exports = {
   i18n: {
     locales: ['default', 'sl', 'en', 'it'],
     defaultLocale: 'default',
     localeDetection: false,
+    localePath: path.resolve('./public/locales'),
   },
 };
